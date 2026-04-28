310945a94a43fbca41ed116aa227cbce2b09f3f8
Dileep Kushwaha <157945764+DileepKushwah@users.noreply.github.com>
2026-04-28 21:01:41 +0530

    Update config.py

--- app/modules/config.py
+++ app/modules/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-GEMINI_MODEL  = "gemini-1.5-flash-001"
+GEMINI_MODEL  = "gemini-1.5-flash"
 EMBED_MODEL   = "models/gemini-embedding-001"
 CHUNK_SIZE    = 1000
 CHUNK_OVERLAP = 200
